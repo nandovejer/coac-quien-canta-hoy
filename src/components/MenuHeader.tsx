143207b69ed54bd7419133c05944194741c72d82
johndoe <johndoe@example.com>
2024-01-10 13:35:15 +0100

    mejora del buscador

--- src/components/MenuHeader.tsx
+++ src/components/MenuHeader.tsx
@@ -23,11 +23,11 @@ const MenuHeader = ({ menuData: { liveUrl, lastDateSession } }: MenuHeaderProps)
                     Ver en Directo
                 </a>
 
-                <a href={"#" + lastDateSession} className="flex justify-center font-medium px-2 py-2 md:px-5 md:py-2 bg-white text-gray-800 border-gray-200 hover:bg-rose-700 hover:text-white text-ellipsis overflow-hidden text-center w-1/3">
-                    Última Sesión {lastDateSession}
+                <a href={"#" + lastDateSession} className="flex justify-center font-medium px-2 py-2 md:px-5 md:py-2 bg-white text-gray-800 border-gray-200 hover:bg-rose-700 hover:text-white text-ellipsis overflow-hidden text-center w-1/3" title={`Última Sesión ` + lastDateSession}>
+                    Última Sesión
                 </a>
                 <button role='link' onClick={scrollToTop} className="flex justify-center font-medium px-2 py-2 md:px-5 md:py-2 bg-white text-gray-800 border-gray-200 hover:bg-rose-700 hover:text-white text-ellipsis overflow-hidden text-center w-1/3">
-                    Ir Arriba
+                    Ir a inicio
                 </button>
             </div>
         </>
